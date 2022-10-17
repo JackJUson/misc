858531af9770cfeb7667ad268e17a2cbab8f0656
Jackson Jung <z5397791@ad.unsw.edu.au>
2022-10-17 23:12:48 +1100

    Add GET request using fetch with React hooks in App.js

--- React/countryapi/src/App.js
+++ React/countryapi/src/App.js
@@ -6,12 +6,22 @@ function App() {
   const [countryData, setCountryData] = useState(null);
 
   const fetchCountryData = (country) => {
+    fetch(`https://restcountries.com/v3/name/${country}`)
+      .then((response) => response.json())
+      .then((data) => {
+
+      });
+      
     console.log(`fetchCountryData function is called with ${country}`)
   }
 
   return (
     <div>
-      <CountryCard country={"something"} area={"something"} population={"something"}/>
+      <CountryCard 
+        country={"something"} 
+        area={"something"} 
+        population={"something"}
+      />
       <SearchBar fetchCountryData={fetchCountryData}/>
     </div>
   );
