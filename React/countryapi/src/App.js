269fd92970448ae1ed53be3e83a614177e1ac613
Jackson Jung <z5397791@ad.unsw.edu.au>
2022-10-17 23:15:07 +1100

    Add countryObj object using setCountryData hook within fetch in App.js

--- React/countryapi/src/App.js
+++ React/countryapi/src/App.js
@@ -9,9 +9,14 @@ function App() {
     fetch(`https://restcountries.com/v3/name/${country}`)
       .then((response) => response.json())
       .then((data) => {
+        const countryData = data[0];
+        const countryObj = {
 
+        };
+        setCountryData(countryObj);
+        console.log(data[0]);
       });
-      
+
     console.log(`fetchCountryData function is called with ${country}`)
   }
 
