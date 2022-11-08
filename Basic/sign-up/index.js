68edebff05ab89e3d11498d3cfe7dcd37a136f64
Jackson Jung <z5397791@ad.unsw.edu.au>
2022-11-08 22:49:45 +1100

    Add event listener for form for submit event in index.js

--- Basic/sign-up/index.js
+++ Basic/sign-up/index.js
@@ -1,4 +1,9 @@
+const form = document.getElementById('form');
 const firstname = document.getElementById('firstname');
 const lastname = document.getElementById('lastname');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
+
+form.addEventListener('submit', e => {
+    
+})
